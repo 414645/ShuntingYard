eca9c947a7be2dc85aa29d5e3e90d7e14fa4edfa
414645 <414645@bsd48.org>
2023-03-06 14:06:01 -0800

    header guard hate me

---
+++ node.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "node.h"
+
+using namespace std;
+
+Node::Node() {
+  cout << "blank node created" << endl;
+}
+
+Node::Node(char newThing) {
+  cout << newThing << " node created" << endl;
+}
+
+Node::~Node() {
+  cout << "node destroyed" << endl;
+}
+
