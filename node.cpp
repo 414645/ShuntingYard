def664b8201d969b18a8265063af2ff751541675
414645 <414645@bsd48.org>
2023-03-06 14:26:15 -0800

    Node (for tree) compiles, still want Lined Listes

--- node.cpp
+++ node.cpp
@@ -5,13 +5,35 @@ using namespace std;
 
 Node::Node() {
   cout << "blank node created" << endl;
+  rightNode = NULL;
+  leftNode = NULL;
+  thing = '0';
 }
 
 Node::Node(char newThing) {
   cout << newThing << " node created" << endl;
+  rightNode = NULL;
+  leftNode = NULL;
+  thing = newThing;
 }
-/*
+
 Node::~Node() {
   cout << "node destroyed" << endl;
 }
-*/
+Node* Node::getRight() {
+  return rightNode;
+}
+Node* Node::getLeft() {
+  return leftNode;
+}
+void Node::setRight(Node* newRight) {
+  rightNode = newRight;
+}
+void Node::setLeft(Node* newLeft) {
+  leftNode = newLeft;
+}
+char Node::getThing() {
+  return thing;
+}
+
+
