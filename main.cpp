d8a665a3453ace7c881ee360b7b7a8c23d0ceb57
414645 <414645@bsd48.org>
2023-03-14 13:35:35 -0700

    Have wifi agian wokring on stack

--- main.cpp
+++ main.cpp
@@ -6,16 +6,42 @@ using namespace std;
 //shunting yard programm
 //Ryan Veith
 
-void push();
+void printLinear (Node* current, Node* newNode);
+
+//Stack (lifo) is built with the idea if it works it works
+void push(Node* &head, Node* current,  Node* newNode);
 char pop();
 char peek();
 
 int main() {
   cout << "hello world" << endl;
+  Node* stackOne = NULL;
+  //cout >> stackOne->getThing() >> endl;
+  Node* a = new Node('1');
+  push(stackOne, stackOne, a);
+  printLinear(stackOne, a);
+
+}
+void printLinear (Node* current, Node* newNode) {
+  if (current != NULL) {
+    //cout << current << endl;
+    cout << " :" << current->getThing() << endl;
+  }
+    else {
+      cout << "current = null" << endl;
+    }
 }
 
-void push() {
-  
+void push(Node* &head, Node* current, Node* newNode) {
+  if (head == NULL) {
+    head = newNode;
+  }
+  else if (current->getRight() == NULL) {
+    current->setRight(newNode);
+  }
+  else {
+    push(head, current->getRight(), newNode);
+  }
 }
 
 char pop() {
